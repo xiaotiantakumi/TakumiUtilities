0fa75f08d0adce17806245b6dc7d3a2e9daa4598
Takumi Oda <xiaotiantakumi@gmail.com>
2019-10-22 22:09:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TakumiUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new StringUtility("aiueo");
+            var ret = temp.StartWith("aiu");
+            Console.Write(ret);
+        }
+    }
+}
---
+++ StringUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakumiUtilities
+{
+    public class StringUtility
+    {
+        public String ValidateTarget { get;}
+        public StringUtility(string validateTarget)
+        {
+            ValidateTarget = validateTarget;
+        }
+
+        public bool StartWith(string target)
+        {
+            return ValidateTarget.StartsWith(target);
+        }
+    }
+}
